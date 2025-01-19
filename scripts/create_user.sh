8286433300eacce8466416190c74046e414b1d15
akira <asa.shakira340@gmail.com>
2025-01-19 20:54:28 +0900

    feat: add create user scripts

---
+++ scripts/create_user.sh
@@ -0,0 +1,2 @@
+curl -X POST -d "@user.json" http://localhost:8080/v1/users
+# curl -X GET http://localhost:8080/v1/users
---
+++ scripts/create_user_data.sh
@@ -0,0 +1,1 @@
+curl -X POST -d "@user_data.json" http://localhost:8080/v1/users
